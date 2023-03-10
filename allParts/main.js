53930d4656bf5cb69b4e29a4498120c7af89451e
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-03-10 15:25:17 +0900

    add error target value

--- allParts/main.js
+++ allParts/main.js
@@ -1,37 +1,40 @@
 // 入力された値をリアルタイムで監視し変数へ代入
 
 const address = document.getElementById("input");
-address.addEventListener('input', handleChange);
+address.addEventListener('change', handleChange); 
+
+let addressText = address.textContent;
 
 function handleChange(event) {
-    const value = event.target.value;
-    console.log(value);
+    sample = event.textContent;
+    console.log(sample);
 }
+handleChange(address);
 // ====================================
 
 // 入力された値が全角か半角かを判定   →  全角だった場合半角へ変換
-function sedAddress() {
-    var number = document.getElementById('address').value;
-    if (checkChar(number) === true) {
-        console.log(`${number} :   全角`);
-        fullWIdth2HalfWidth(number);
-        console.log(`${number} : 全角 → 半角変更後`);
-    } else {
-        console.log(`${number} :  半角`);
-    }
-}
+// function sedAddress() {
+//     var number = document.getElementById('address').value;
+//     if (checkChar(number) === true) {
+//         console.log(`${number} :   全角`);
+//         fullWIdth2HalfWidth(number);
+//         console.log(`${number} : 全角 → 半角変更後`);
+//     } else {
+//         console.log(`${number} :  半角`);
+//     }
+// }
 
     // 入力された値が半角か全角かを判定する関数
-    function checkChat(input) {
-        var regex = /^[^\x01-\x7E]*$/;
-        return regex.test(input);
-    }
+    // function checkChat(input) {
+    //     var regex = /^[^\x01-\x7E]*$/;
+    //     return regex.test(input);
+    // }
     // 全角を半角へ変換する関数
-    function fullWIdth2HalfWidth(src) {
-        return src.replace(/[A-Za-z0-9]/g, function(s) {
-            return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-        });
-    }
+    // function fullWIdth2HalfWidth(src) {
+    //     return src.replace(/[A-Za-z0-9]/g, function(s) {
+    //         return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    //     });
+    // }
 
 // ====================================
 // 住所検索API
