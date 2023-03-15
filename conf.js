5515f0d67955d91a344ea8384f7e01ffd973bb0c
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-03-15 17:34:18 +0900

    add all files

---
+++ conf.js
@@ -0,0 +1,14 @@
+export class CONSTRUCTOR {
+    constructor() {
+      return {
+        PCJP_APIKEY: 'wtcwgnnjK3v31kmtCvupRHIIgY5lCDnLEroDw3v',
+        PCJP_API_ENDPOINT: 'https://apis.postcode-jp.com/api/v4/',
+        // API_REQUEST_URL: '/api/v1/entry_point.php',
+        API_REQUEST_URL: '/ep_0014/api/entry',
+        RESULT_URL: '/ep_0014/entry/result',
+        RESULT_URL_NON_FIRING: '/ep_0014/entry/thanks',
+        CONFIRM_ADDRESS: true
+      }
+    }
+  }
+  
