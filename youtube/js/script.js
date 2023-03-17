c1501629dc9e693fc415521e039fe22bfae50c9d
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-03-17 19:41:26 +0900

    add script fetch

--- youtube/js/script.js
+++ youtube/js/script.js
@@ -1,9 +1,29 @@
-// 関数処理を行う関数を定義
-const synchrounsFunc = () => {
-    console.log("これは同期処理関数内のログです。");
-    return "完了!";
-}
+new Promise(function (resolve, reject) {
+    try {
+        // 非同期処理
+        resolve();
+    } catch (e) {
+        // 異常時終了の処理
 
-const message = "同期処理　";
-const result = synchrounsFunc();
-console.log(message + result);
+        // return の代わりに以上終了したことを表すrejectを返す
+        reject();
+    }
+})
+
+
+
+
+
+
+
+
+// 関数処理を行う関数を定義
+// const synchrounsFunc = () => {
+//     console.log("これは同期処理関数内のログです。");
+//     return "完了!";
+// }
+
+// const message = "同期処理　";
+// const result = synchrounsFunc();
+// console.log(message + result);
+
