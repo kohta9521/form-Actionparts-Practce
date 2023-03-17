f90fffd9adb1a4790fd1d10e0b0b5d9ce0553940
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-03-17 20:09:22 +0900

    add last commit

--- youtube/js/script.js
+++ youtube/js/script.js
@@ -1,29 +1,44 @@
-new Promise(function (resolve, reject) {
-    try {
-        // 非同期処理
-        resolve();
-    } catch (e) {
-        // 異常時終了の処理
+function asyncFunction() {
+    return new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => {
+                console.log("非同期処理")
 
-        // return の代わりに以上終了したことを表すrejectを返す
-        reject();
-    }
+                resolve();
+            }, 1000)
+        } catch (e) {
+            reject();
+        }
+    })
+}
+
+asyncFunction().then(() => {
+    console.log("recolve後の処理");
+}).catch(e => {
+    console.log("reject後の処理");
 })
 
+function asyncFunction() {
+    return new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => {
+                console.log("非同期処理")
+                const num = 1
+                resolve(num)
+            }, 1000)
+        }
+    })
+}
 
 
+// new Promise(function (resolve, reject) {
+//     try {
+//         setTimeout(() => {
+//             console.log("非同期処理")
 
-
-
-
-
-// 関数処理を行う関数を定義
-// const synchrounsFunc = () => {
-//     console.log("これは同期処理関数内のログです。");
-//     return "完了!";
-// }
-
-// const message = "同期処理　";
-// const result = synchrounsFunc();
-// console.log(message + result);
-
+//             resolve()
+//         }, 1000)
+//     } catch (e) {
+//         reject()
+//     }
+// })
